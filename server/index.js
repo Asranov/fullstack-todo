bb61ab2f831c942f098528c6d701d55f14659f4e
Asranov <asranov0003@gmail.com>
2024-02-21 10:34:08 +0500

    core: install dot env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+require('dotenv').config();
 
 const app = express();
+
+console.log(process.env.DB)
 
 app.listen(8080, () =>
   console.log("Server listening on http://localhost:8080")
